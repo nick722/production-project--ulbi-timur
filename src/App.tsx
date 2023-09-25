2ee61afddc9e0763f4bff23711f74c5fd9298a8c
Nick <nick22783@gmail.com>
2023-09-25 16:05:04 +0300

    6 Роутинг Code splitting Lazy Suspence from 1. Webpack. Введение в арх-ру. Code splitting. Css Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import "./index.scss"
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
   return (
     <div className="app">
-      Keep calm and code on
-      <Counter/>
+      <Link to={`/`}>Home</Link>
+      <Link to={`/about`}>About Us</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={`/about`} element={<AboutPageAsync/>}/>
+          <Route path={`/`} element={<MainPageAsync/>}/>
+        </Routes>
+      </Suspense>
+
     </div>
   );
 };
